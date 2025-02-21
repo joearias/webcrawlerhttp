67cb9d161b03977cd4748dcc2ee37a73f9e30097
Joe Arias <joearias@gmail.com>
2025-02-20 23:18:10 -0600

    Update to main

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
-console.log("hello worldoO0");
+function main(){
+
+    if(process.argv.length< 3){
+        console.log('No website provided');
+        process.exit(1);
+    }
+    if (process.argv.length > 3) {
+        console.log('Too many commnad line args');
+        process.exit(1);
+    }
+    const baseURL = process.argv[2];
+
+    console.log(`Starting Crawl ${baseURL}`);
+}
+
+main();
